558f5f6f36e7cd3fa99d58e95f0f35da7a3e785a
Victor Matias <fvictor1705@gmail.com>
2022-08-16 19:51:20 -0300

    Resolve requisito 4.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Aplica casos de teste em NotFound.js', () => {
+  it('Verifica se na página existe um Heading com texto desejado', () => {
+    render(<NotFound />);
+
+    const heading = screen.getByRole('heading', {
+      level: 2, name: /Page requested not found/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verifica se na página existe a imagem desejada', () => {
+    render(<NotFound />);
+
+    const img = screen.getByAltText(/pikachu crying/i);
+
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
